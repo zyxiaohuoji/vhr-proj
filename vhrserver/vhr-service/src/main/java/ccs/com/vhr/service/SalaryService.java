48cd8f86b67606cd259d6c5ca74ac4bd2053e6b6
zyxiaohuoji <1275819010@qq.com>
2020-12-23 15:51:44 +0800

    工资套账添加

---
+++ vhrserver/vhr-service/src/main/java/ccs/com/vhr/service/SalaryService.java
@@ -0,0 +1,33 @@
+package ccs.com.vhr.service;
+
+import ccs.com.vhr.mapper.SalaryMapper;
+import ccs.com.vhr.model.Salary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class SalaryService {
+
+    @Autowired
+    SalaryMapper salaryMapper;
+
+    public List<Salary> getAllSalaries() {
+        return salaryMapper.getAllSalaries();
+    }
+
+    public Integer addSalary(Salary salary) {
+        salary.setCreateDate(new Date());
+        return salaryMapper.insertSelective(salary);
+    }
+
+    public Integer deleteSalaryById(Integer id) {
+        return salaryMapper.deleteByPrimaryKey(id);
+    }
+
+    public Integer updateSalary(Salary salary) {
+        return salaryMapper.updateByPrimaryKeySelective(salary);
+    }
+}
